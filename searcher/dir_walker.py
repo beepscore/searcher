429510e2fcf7ad5657903e90f32b6b0e4f4635c0
beepscore <support@beepscore.com>
2016-03-19 23:34:34 -0700

    Add dir_walker.py and test_dir_walker.py

---
+++ searcher/dir_walker.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+
+
+class DirWalker:
+    """
+    Walks directory
+
+    http://stackoverflow.com/questions/954504/how-to-get-files-in-a-directory-including-all-subdirectories
+    https://ssscripting.wordpress.com/2009/03/03/python-recursive-directory-walker/
+    """
+
+    @staticmethod
+    def files_in_dir_recursive(dir):
+        """ return list of files in dir and subdirectories """
+        file_paths = []
+        for dirpath, dirnames, filenames in os.walk(dir):
+            for filename in filenames:
+                full_name = os.path.join(dirpath, filename)
+                file_paths.append(full_name)
+        return file_paths
+
+
+#    def walk_files_in_dir(dir, method):
+#        """ walks a directory, and executes a method on each file """
+#        dir = os.path.abspath(dir)
+#        for file in [file for file in os.listdir(dir) if not file in [".",".."]]:
+#            nfile = os.path.join(dir,file)
+#                method(nfile)
+#                if os.path.isdir(nfile):
+#                    self.walk(nfile,method)
---
+++ tests/test_dir_walker.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import unittest
+
+from searcher import dir_walker
+
+
+class TestDirWalker(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        walker = dir_walker.DirWalker()
+        self.assertIsNotNone(walker)
+
+    def test_files_in_dir_recursive(self):
+        actual = dir_walker.DirWalker.files_in_dir_recursive("./searcher_data/search_dir")
+
+        expected = ['./searcher_data/search_dir/.DS_Store',
+                './searcher_data/search_dir/httppython.org',
+                './searcher_data/search_dir/httpsen.wikipedia.orgwikiPython_%28programming_language%29',
+                './searcher_data/search_dir/httpswww.google.com#q=python',
+                './searcher_data/search_dir/httpwww.beepscore.comhubcape']
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
