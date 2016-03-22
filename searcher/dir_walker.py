5a20bea21b83538a84970bac6c2ea26dd6c43c16
beepscore <support@beepscore.com>
2016-03-22 12:46:13 -0700

    Move two methods from dir_walker into file_helper. Add test_file_helper.py and move tests into it.
    Note SourceTree is not showing preview for file_helper.py or test_file_helper.py. Is this a bug, or do I need to change UI settings?
    All unit tests pass.

--- searcher/dir_walker.py
+++ searcher/dir_walker.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import os.path
-from searcher import expression_helper
+from searcher import file_helper
 from searcher import expression_searcher
 
 
@@ -10,84 +8,7 @@ class DirWalker:
     """
     Walks directory
 
-    http://stackoverflow.com/questions/954504/how-to-get-files-in-a-directory-including-all-subdirectories
-    https://ssscripting.wordpress.com/2009/03/03/python-recursive-directory-walker/
     """
-
-    @staticmethod
-    def directories_in_dir_recursive(search_dir, ignored_regex_objects):
-        """
-        Searches search_dir and subdirectories for directories
-
-        Ignores symlinks. Doesn't ignore alias.
-        http://apple.stackexchange.com/questions/2991/whats-the-difference-between-alias-and-link
-
-        param search_dir is the directory to search
-        param ignored_regex_objects contains regular expression objects compiled from patterns
-        return list of un-ignored directories in search_dir and subdirectories
-        """
-
-        dir_paths = [search_dir]
-
-        for dirpath, dirnames, filenames in os.walk(search_dir):
-
-                for dirname in dirnames:
-
-                    if expression_helper.ExpressionHelper.is_string_matched_in_regular_expression_objects(dirpath,
-                                                                                                          ignored_regex_objects):
-                        # ignore subdirectories of ignored directory
-                        continue
-
-                    if os.path.islink(dirname):
-                        # ignore symlink
-                        # http://stackoverflow.com/questions/15718006/check-if-directory-is-symlink
-                        continue
-
-                    if expression_helper.ExpressionHelper.is_string_matched_in_regular_expression_objects(dirname,
-                                                                                                          ignored_regex_objects):
-                        # ignore this directory
-                        continue
-
-                    full_name = os.path.join(dirpath, dirname)
-                    dir_paths.append(full_name)
-
-        return dir_paths
-
-    @staticmethod
-    def files_in_dir(search_dir, ignored_regex_objects):
-        """
-        Searches search_dir for files
-
-        Ignores symlinks. Doesn't ignore alias.
-        http://apple.stackexchange.com/questions/2991/whats-the-difference-between-alias-and-link
-
-        param ignored_regex_objects contains regular expression objects compiled from patterns
-        return list of un-ignored files in search_dir, relative to search_dir
-        """
-
-        file_paths = []
-        dir_list = os.listdir(search_dir)
-        for filename in dir_list:
-
-            search_dir_abspath = os.path.abspath(search_dir)
-            full_name = os.path.join(search_dir_abspath, filename)
-            if os.path.isdir(full_name):
-                # ignore directory
-                continue
-
-            if os.path.islink(full_name):
-                # ignore symlink
-                # http://stackoverflow.com/questions/15718006/check-if-directory-is-symlink
-                continue
-
-            if expression_helper.ExpressionHelper.is_string_matched_in_regular_expression_objects(filename,
-                                                                                                  ignored_regex_objects):
-                # ignore this file
-                continue
-
-            file_paths.append(filename)
-
-        return file_paths
 
     @staticmethod
     def directories_number_of_files_containing_keyword(root_dir, ignored_regex_objects, keyword):
@@ -98,14 +19,14 @@ class DirWalker:
         return dictionary with key directory name and value number of files that contain expression
         """
 
-        directories = DirWalker.directories_in_dir_recursive(root_dir, ignored_regex_objects)
+        directories = file_helper.FileHelper.directories_in_dir_recursive(root_dir, ignored_regex_objects)
         results = {}
 
         for directory in directories:
 
             number_of_files_containing_expression = 0
 
-            filenames = DirWalker.files_in_dir(directory, ignored_regex_objects)
+            filenames = file_helper.FileHelper.files_in_dir(directory, ignored_regex_objects)
 
             for filename in filenames:
 
--- searcher/file_helper.py
+++ searcher/file_helper.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import os
+from searcher import expression_helper
 
 
 class FileHelper:
-    """ Utility methods for absolute paths """
+    """ Utility methods for working with files and directories
+
+    http://stackoverflow.com/questions/954504/how-to-get-files-in-a-directory-including-all-subdirectories
+    https://ssscripting.wordpress.com/2009/03/03/python-recursive-directory-walker/
+    """
 
     @staticmethod
     def absolute_dir_path(dirname):
@@ -20,4 +25,79 @@ class FileHelper:
         absolute_dir_path = FileHelper.absolute_dir_path(dirname)
         absolute_file_path = os.path.join(absolute_dir_path, filename)
         return absolute_file_path
+
+    @staticmethod
+    def directories_in_dir_recursive(search_dir, ignored_regex_objects):
+        """
+        Searches search_dir and subdirectories for directories
+
+        Ignores symlinks. Doesn't ignore alias.
+        http://apple.stackexchange.com/questions/2991/whats-the-difference-between-alias-and-link
+
+        param search_dir is the directory to search
+        param ignored_regex_objects contains regular expression objects compiled from patterns
+        return list of un-ignored directories in search_dir and subdirectories
+        """
+
+        dir_paths = [search_dir]
+
+        for dirpath, dirnames, filenames in os.walk(search_dir):
+
+            for dirname in dirnames:
+
+                if expression_helper.ExpressionHelper.is_string_matched_in_regular_expression_objects(dirpath,
+                                                                                                      ignored_regex_objects):
+                    # ignore subdirectories of ignored directory
+                    continue
+
+                if os.path.islink(dirname):
+                    # ignore symlink
+                    # http://stackoverflow.com/questions/15718006/check-if-directory-is-symlink
+                    continue
+
+                if expression_helper.ExpressionHelper.is_string_matched_in_regular_expression_objects(dirname,
+                                                                                                      ignored_regex_objects):
+                    # ignore this directory
+                    continue
+
+                full_name = os.path.join(dirpath, dirname)
+                dir_paths.append(full_name)
+
+        return dir_paths
+
+    @staticmethod
+    def files_in_dir(search_dir, ignored_regex_objects):
+        """
+        Searches search_dir for files
+
+        Ignores symlinks. Doesn't ignore alias.
+        http://apple.stackexchange.com/questions/2991/whats-the-difference-between-alias-and-link
+
+        param ignored_regex_objects contains regular expression objects compiled from patterns
+        return list of un-ignored files in search_dir, relative to search_dir
+        """
+
+        file_paths = []
+        dir_list = os.listdir(search_dir)
+        for filename in dir_list:
+
+            search_dir_abspath = os.path.abspath(search_dir)
+            full_name = os.path.join(search_dir_abspath, filename)
+            if os.path.isdir(full_name):
+                # ignore directory
+                continue
+
+            if os.path.islink(full_name):
+                # ignore symlink
+                # http://stackoverflow.com/questions/15718006/check-if-directory-is-symlink
+                continue
+
+            if expression_helper.ExpressionHelper.is_string_matched_in_regular_expression_objects(filename,
+                                                                                                  ignored_regex_objects):
+                # ignore this file
+                continue
+
+            file_paths.append(filename)
+
+        return file_paths
 
