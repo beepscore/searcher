53fadc9bf7a05b5f4a36feb78ece65b7cdf2e278
beepscore <support@beepscore.com>
2016-03-23 15:22:23 -0700

    Use pprint to pretty print results. Change indentation from 8 to 4 spaces.

--- searcher/search_expression.py
+++ searcher/search_expression.py
@@ -4,6 +4,8 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..', 'searcher')))
 
+import pprint
+
 from searcher import searcher_arg_reader
 
 from searcher import expression_helper
@@ -12,24 +14,25 @@ from searcher import expression_searcher
 
 if __name__ == '__main__':
 
-        """
-        Search for expression without instantiating an instance. Use command line arguments.
-        """
+    """
+    Search for expression without instantiating an instance. Use command line arguments.
+    """
 
-        # instantiate arg_reader
-        arg_reader = searcher_arg_reader.SearcherArgReader()
+    # instantiate arg_reader
+    arg_reader = searcher_arg_reader.SearcherArgReader()
 
-        # Call arg_reader.args() without an argument list so it reads from command line.
-        args = arg_reader.args()
+    # Call arg_reader.args() without an argument list so it reads from command line.
+    args = arg_reader.args()
 
-        expression = args.keyword
-        search_dir = args.root_dir
-        print "Searching root_dir " + search_dir + " for keyword " + expression
+    expression = args.keyword
+    search_dir = args.root_dir
+    print "Searching root_dir " + search_dir + " for keyword " + expression
 
-        ignored_filename_patterns = ['\A\.$', '\A\.\.$', '\A\.DS_Store$']
-        ignored_regex_objects = expression_helper.ExpressionHelper.regex_objects_from_patterns(ignored_filename_patterns)
+    ignored_filename_patterns = ['\A\.$', '\A\.\.$', '\A\.DS_Store$']
+    ignored_regex_objects = expression_helper.ExpressionHelper.regex_objects_from_patterns(ignored_filename_patterns)
 
-        results = expression_searcher.ExpressionSearcher.directories_number_of_files_containing_keyword(search_dir,
-                                                                                                        ignored_regex_objects, expression)
-        print "Results"
-        print results
+    results = expression_searcher.ExpressionSearcher.directories_number_of_files_containing_keyword(search_dir,
+                                                                                                    ignored_regex_objects, expression)
+    print "Results"
+    prettyprinter = pprint.PrettyPrinter(indent=4)
+    prettyprinter.pprint(results)
