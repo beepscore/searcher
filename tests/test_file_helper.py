5ab05def3561d481d441b419676cbea58510584d
beepscore <support@beepscore.com>
2018-12-25 21:41:05 -0800

    Update test_directories_in_dir_recursive_ignore2 to work on Windows

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -68,14 +68,14 @@ class TestFileHelper(unittest.TestCase):
         ignored_dirname_patterns = ['level_2']
         ignored_regex_objects = expression_helper.regex_objects_from_patterns(ignored_dirname_patterns)
 
-        actual = file_helper.directories_in_dir_recursive("./searcher_data/search_dir",
-                                                          ignored_regex_objects)
+        search_dir_full_path = os.path.join('.', 'searcher_data', 'search_dir')
+        actual = file_helper.directories_in_dir_recursive(search_dir_full_path, ignored_regex_objects)
 
         # Don't care about element order, so compare results using set instead of list
-        expected = {'./searcher_data/search_dir',
-                    './searcher_data/search_dir/level_1',
-                    './searcher_data/search_dir/level_1/.git_fake',
-                    './searcher_data/search_dir/level_1/.git_fake/objects_fake',
+        expected = {os.path.join('.', 'searcher_data', 'search_dir'),
+                    os.path.join('.', 'searcher_data', 'search_dir', 'level_1'),
+                    os.path.join('.', 'searcher_data', 'search_dir', 'level_1', '.git_fake'),
+                    os.path.join('.', 'searcher_data', 'search_dir', 'level_1', '.git_fake', 'objects_fake')
                     }
         self.assertEqual(expected, set(actual))
 
