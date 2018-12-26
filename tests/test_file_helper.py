d2ec65edef5f9b0d06c82be2d513609fd89b1db0
beepscore <support@beepscore.com>
2018-12-25 21:46:49 -0800

    Update test_files_in_dir_level_1 to work in Windows

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -130,8 +130,8 @@ class TestFileHelper(unittest.TestCase):
 
         ignored_regex_objects = expression_helper.regex_objects_from_patterns(expression_helper.ignored_filename_patterns)
 
-        actual = file_helper.files_in_dir("./searcher_data/search_dir/level_1",
-                                          ignored_regex_objects)
+        search_dir_full_path = os.path.join('.', 'searcher_data', 'search_dir', 'level_1')
+        actual = file_helper.files_in_dir(search_dir_full_path, ignored_regex_objects)
 
         # Don't care about element order, so compare results using set instead of list
         expected = {'a.txt', 'c.txt alias'}
