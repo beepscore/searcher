5e75368d62e60e25a884d978a5a88bf4a8248bf5
beepscore <support@beepscore.com>
2018-12-25 23:45:46 -0800

    Add test_files_in_dir_recursive_ignore_ython

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -209,6 +209,30 @@ class TestFileHelper(unittest.TestCase):
 
         self.assertEqual(expected, set(actual))
 
+    def test_files_in_dir_recursive_ignore_ython(self):
+
+        ignored_filename_patterns = [r'\A\.$', r'\A\.\.$', r'\A\.DS_Store$', r'ython']
+        ignored_regex_objects = expression_helper.regex_objects_from_patterns(ignored_filename_patterns)
+
+        search_dir_full_path = os.path.join('.', 'searcher_data', 'search_dir')
+        actual = file_helper.files_in_dir_recursive(search_dir_full_path, ignored_regex_objects)
+
+        # Don't care about element order, so compare results using set instead of list
+        expected = {
+            os.path.join('.', 'searcher_data', 'search_dir', 'httpwww.beepscore.comhubcape'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'level_2', 'level_3', 'level_4', 'test_result01.txt'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', '.git_fake', 'objects_fake', 'object_fake'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'c.txt alias'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'something.exe'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'a.txt'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'level_2', 'c.txt'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'level_2', 'b.txt'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'level_2', 'd.txt'),
+            os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'level_2', 'level_3', 'd.txt alias')
+        }
+
+        self.assertEqual(expected, set(actual))
+
 
 if __name__ == "__main__":
     unittest.main()
