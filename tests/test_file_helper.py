d31297478c8ef7c229fe2c73bb2fb8465ce9beb9
beepscore <support@beepscore.com>
2019-08-20 21:52:49 -0700

    use pathlib. all unit tests pass

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -152,7 +152,9 @@ class TestFileHelper(unittest.TestCase):
         ignored_filename_patterns = [r'\A\.$', r'\A\.\.$', r'\A\.DS_Store$']
         ignored_regex_objects = expression_helper.regex_objects_from_patterns(ignored_filename_patterns)
 
-        search_dir_full_path = os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'level_2')
+        # search_dir_full_path = os.path.join('.', 'searcher_data', 'search_dir', 'level_1', 'level_2')
+        search_dir_full_path = Path('.').joinpath('searcher_data', 'search_dir', 'level_1', 'level_2')
+
         actual = file_helper.files_in_dir(search_dir_full_path, ignored_regex_objects)
 
         # Don't care about element order, so compare results using set instead of list
