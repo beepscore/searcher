5e839e9259f3d105c5d9f58a2b6487502e249d5f
beepscore <support@beepscore.com>
2019-08-25 08:11:05 -0700

    Add test_paths_in_dir_set_from_reordered_list. All 46 unit tests pass.

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -476,6 +476,25 @@ class TestFileHelper(unittest.TestCase):
         }
         self.assertEqual(expected, set(actual))
 
+    def test_paths_in_dir_set_from_reordered_list(self):
+        """ test we are using set correctly. """
+
+        ignored_filename_patterns = [r'\A\.$', r'\A\.\.$', r'\A\.DS_Store$']
+        ignored_regex_objects = expression_helper.regex_objects_from_patterns(ignored_filename_patterns)
+
+        search_dir_full_path =pathlib.Path('.').joinpath('searcher_data', 'search_dir')
+
+        actual = file_helper.paths_in_dir(search_dir_full_path, ignored_regex_objects)
+
+        # Don't care about element order, so compare results using set instead of list
+        expected = {
+            search_dir_full_path.joinpath('httpsen.wikipedia.orgwikiPython_%28programming_language%29'),
+            search_dir_full_path.joinpath('httpswww.google.com#q=python'),
+            search_dir_full_path.joinpath('httpwww.beepscore.comhubcape'),
+            search_dir_full_path.joinpath('httppython.org')
+        }
+        self.assertEqual(expected, set(actual))
+
 
 if __name__ == "__main__":
     unittest.main()
