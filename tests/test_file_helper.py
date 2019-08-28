90fb1ddde667e525c1f9ac3d79929a1ce40ae2a4
beepscore <support@beepscore.com>
2019-08-27 21:18:06 -0700

    add test_paths_in_dir_recursive_ignore_ython

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -539,6 +539,34 @@ class TestFileHelper(unittest.TestCase):
 
         self.assertEqual(expected, set(actual))
 
+    def test_paths_in_dir_recursive_ignore_ython(self):
+        ignored_directory_patterns = [r'\A\.$', r'\A\.\.$']
+        ignored_directory_regex_objects = expression_helper.regex_objects_from_patterns(ignored_directory_patterns)
+
+        ignored_filename_patterns = [r'\A\.DS_Store$', r'ython']
+        ignored_file_regex_objects = expression_helper.regex_objects_from_patterns(ignored_filename_patterns)
+
+        search_dir_full_path = pathlib.Path('.').joinpath('searcher_data', 'search_dir')
+        actual = file_helper.paths_in_dir_recursive(search_dir_full_path,
+                                                    ignored_directory_regex_objects,
+                                                    ignored_file_regex_objects)
+
+        # Don't care about element order, so compare results using set instead of list
+        expected = {
+            search_dir_full_path.joinpath('httpwww.beepscore.comhubcape'),
+            search_dir_full_path.joinpath('level_1', 'level_2', 'level_3', 'level_4', 'test_result01.txt'),
+            search_dir_full_path.joinpath('level_1', '.git_fake', 'objects_fake', 'object_fake'),
+            search_dir_full_path.joinpath('level_1', 'c.txt alias'),
+            search_dir_full_path.joinpath('level_1', 'something.exe'),
+            search_dir_full_path.joinpath('level_1', 'a.txt'),
+            search_dir_full_path.joinpath('level_1', 'level_2', 'c.txt'),
+            search_dir_full_path.joinpath('level_1', 'level_2', 'b.txt'),
+            search_dir_full_path.joinpath('level_1', 'level_2', 'd.txt'),
+            search_dir_full_path.joinpath('level_1', 'level_2', 'level_3', 'd.txt alias')
+        }
+
+        self.assertEqual(expected, set(actual))
+
     def test_paths_in_dir_recursive_level_2(self):
         ignored_directory_patterns = [r'\A\.$', r'\A\.\.$']
         ignored_directory_regex_objects = expression_helper.regex_objects_from_patterns(ignored_directory_patterns)
